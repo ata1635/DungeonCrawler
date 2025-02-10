9ff89f2a135e9dd25814ec272f9f951634e6b18d
ata1635 <atakan_berk.rana@hotmail.com>
2025-02-10 13:42:05 +0100

    fixed indent

--- DungeonCrawler.java
+++ DungeonCrawler.java
@@ -10,7 +10,7 @@ public class DungeonCrawler {
 	
 	public static void main(String args[]) {
 		StdDraw.setXscale(0, 100);
-        StdDraw.setYscale(0, 100);
+		StdDraw.setYscale(0, 100);
 		int roomNumber = 7;
 		DungeonRoom[][] dungeonRooms = new DungeonRoom[roomNumber][5];
 		generate(dungeonRooms);
